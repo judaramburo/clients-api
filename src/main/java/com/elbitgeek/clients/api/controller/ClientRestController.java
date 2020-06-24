49f9c9f14e153d7c26eee697695f900cb38bc87e
judaramburo <judaramburo@gmail.com>
2020-06-24 17:12:46 -0500

    Add handler error not found

--- src/main/java/com/elbitgeek/clients/api/controller/ClientRestController.java
+++ src/main/java/com/elbitgeek/clients/api/controller/ClientRestController.java
@@ -1,9 +1,13 @@
 package com.elbitgeek.clients.api.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,8 +36,25 @@ public class ClientRestController {
 	}
 	
 	@GetMapping("/clients/{id}")
-	public Client show(@PathVariable Long id) {
-		return clientService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		
+		Client client = null;
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		try {
+			client = clientService.findById(id);
+		}catch(DataAccessException e) {
+			response.put("message", "Server Error");
+			response.put("error", e.getMostSpecificCause());
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if(client == null) {
+			response.put("message", "Client id not found");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Client>(client, HttpStatus.OK);
 	}
 	
 	@PostMapping("/clients")
