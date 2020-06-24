1b45f94c96c5abdd3b10fd9f65151b35dde5a730
judaramburo <judaramburo@gmail.com>
2020-06-24 10:18:57 -0500

    Add all CRUD methods

--- src/main/java/com/elbitgeek/clients/api/controller/ClientRestController.java
+++ src/main/java/com/elbitgeek/clients/api/controller/ClientRestController.java
@@ -3,9 +3,16 @@ package com.elbitgeek.clients.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.elbitgeek.clients.api.entity.Client;
@@ -22,5 +29,33 @@ public class ClientRestController {
 	@GetMapping("/clients")
 	public List<Client> index(){
 		return clientService.findAll();
+	}
+	
+	@GetMapping("/clients/{id}")
+	public Client show(@PathVariable Long id) {
+		return clientService.findById(id);
+	}
+	
+	@PostMapping("/clients")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client create(@RequestBody Client client) {
+		return clientService.save(client);
+	}
+	
+	@PutMapping("/clients/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client update(@RequestBody Client newClient, @PathVariable Long id) {
+		Client client = clientService.findById(id);
+		client.setFirstName(newClient.getFirstName());
+		client.setLastName(newClient.getLastName());
+		client.setEmail(newClient.getEmail());
+		
+		return clientService.save(client);
+	}
+	
+	@DeleteMapping("/clients/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clientService.delete(id);
 	}
 }
--- src/main/java/com/elbitgeek/clients/api/services/IClientService.java
+++ src/main/java/com/elbitgeek/clients/api/services/IClientService.java
@@ -7,5 +7,11 @@ import com.elbitgeek.clients.api.entity.Client;
 public interface IClientService {
 
 	public List<Client> findAll();
+	
+	public Client findById(Long id);
+	
+	public Client save(Client client);
+	
+	public void delete(Long id);
 
 }
